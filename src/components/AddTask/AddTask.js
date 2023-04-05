fd80a680206e81e10fdfbccd1a0c25f4d0be22cf
Israfil Hossen <sisrafilss@gmail.com>
2023-04-05 09:52:27 +0600

    add task modal added

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,9 +1,16 @@
 import React from "react";
+import closeIcon from "../../img/close.png";
 
-const AddTask = () => {
+const AddTask = ({ toggleModal }) => {
   return (
-    <div className="flex flex-col w-1/3 mx-auto mt-5">
-      <h1 className="mt-4 mb-3 text-2xl font-medium">Add Task</h1>
+    <div className="flex flex-col w-full bg-white py-8 px-12 rounded relative">
+      <h2 className="mb-3  text-2xl font-medium">Add Task</h2>
+      <img
+        onClick={toggleModal}
+        src={closeIcon}
+        className="w-3 h-3 absolute top-4 right-4 cursor-pointer"
+        alt=""
+      />
       <form className="flex flex-col ">
         <input
           className="border px-3 text-black opacity-90 py-2 mb-5 rounded w-full focus:outline-none"
@@ -24,6 +31,7 @@ const AddTask = () => {
         <button
           type="submit"
           className="px-2 py-2 bg-slate-600 text-white rounded font-medium text-base"
+          // onClick={toggleModal}
         >
           Add
         </button>
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,120 +1,142 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TaskList.css";
 import update from "../../img/edit.png";
 import dlete from "../../img/delete.png";
+import AddTask from "../AddTask/AddTask";
 
 const TaskList = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
   return (
-    <div className="w-2/3 mx-auto mt-16">
-      <div className="flex justify-between ">
-        <h2 className="text-2xl font-bold">Task List</h2>
-        <button className="px-2 py-2 bg-slate-600 text-white rounded font-medium text-base">
-          Add New Task
-        </button>
+    <>
+      <div className="w-2/3 mx-auto mt-16">
+        <div className="flex justify-between ">
+          <h2 className="text-2xl font-bold">Task List</h2>
+          <button
+            onClick={toggleModal}
+            className="px-2 py-2 bg-slate-600 text-white rounded font-medium text-base"
+          >
+            Add New Task
+          </button>
+        </div>
+
+        <table className="table-auto mt-6 w-full">
+          <thead className="bg-gray-200">
+            <tr className="border">
+              <th className="px-6 py-3 border ">#</th>
+              <th className="px-6 py-3 border">Title</th>
+              <th className="px-6 py-3 border">Description</th>
+              <th className="px-6 py-3 border">Due Date</th>
+              <th className="px-6 py-3 border">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="cursor-pointer">
+              <td className="px-6 py-3 border ">1</td>
+              <td className="px-6 py-3 border ">Hefzul Quran</td>
+              <td className="px-6 py-3 border ">
+                Review the 1st para. First recite ...
+              </td>
+              <td className="px-6 py-3 border ">5 Apr 2023</td>
+              <td className="px-6 py-3 border flex justify-between">
+                <img
+                  className="w-6 h-6 cursor-pointer mr-2"
+                  src={update}
+                  alt=""
+                  title="Edit"
+                />
+                <img
+                  className="w-6 h-6 cursor-pointer "
+                  src={dlete}
+                  alt=""
+                  title="Delete"
+                />
+              </td>
+            </tr>
+            <tr className="cursor-pointer">
+              <td className="px-6 py-3 border ">1</td>
+              <td className="px-6 py-3 border ">Hefzul Quran</td>
+              <td className="px-6 py-3 border ">
+                Review the 1st para. First recite ...
+              </td>
+              <td className="px-6 py-3 border ">5 Apr 2023</td>
+              <td className="px-6 py-3 border flex justify-between">
+                <img
+                  className="w-6 h-6 cursor-pointer mr-2"
+                  src={update}
+                  alt=""
+                  title="Edit"
+                />
+                <img
+                  className="w-6 h-6 cursor-pointer "
+                  src={dlete}
+                  alt=""
+                  title="Delete"
+                />
+              </td>
+            </tr>
+            <tr className="cursor-pointer">
+              <td className="px-6 py-3 border ">1</td>
+              <td className="px-6 py-3 border ">Hefzul Quran</td>
+              <td className="px-6 py-3 border ">
+                Review the 1st para. First recite ...
+              </td>
+              <td className="px-6 py-3 border ">5 Apr 2023</td>
+              <td className="px-6 py-3 border flex justify-between">
+                <img
+                  className="w-6 h-6 cursor-pointer mr-2"
+                  src={update}
+                  alt=""
+                  title="Edit"
+                />
+                <img
+                  className="w-6 h-6 cursor-pointer "
+                  src={dlete}
+                  alt=""
+                  title="Delete"
+                />
+              </td>
+            </tr>
+            <tr className="cursor-pointer">
+              <td className="px-6 py-3 border ">1</td>
+              <td className="px-6 py-3 border ">Hefzul Quran</td>
+              <td className="px-6 py-3 border ">
+                Review the 1st para. First recite ...
+              </td>
+              <td className="px-6 py-3 border ">5 Apr 2023</td>
+              <td className="px-6 py-3 border flex justify-between">
+                <img
+                  className="w-6 h-6 cursor-pointer mr-2"
+                  src={update}
+                  alt=""
+                  title="Edit"
+                />
+                <img
+                  className="w-6 h-6 cursor-pointer "
+                  src={dlete}
+                  alt=""
+                  title="Delete"
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
 
-      <table className="table-auto mt-6 w-full">
-        <thead className="bg-gray-200">
-          <tr className="border">
-            <th className="px-6 py-3 border ">#</th>
-            <th className="px-6 py-3 border">Title</th>
-            <th className="px-6 py-3 border">Description</th>
-            <th className="px-6 py-3 border">Due Date</th>
-            <th className="px-6 py-3 border">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="cursor-pointer">
-            <td className="px-6 py-3 border ">1</td>
-            <td className="px-6 py-3 border ">Hefzul Quran</td>
-            <td className="px-6 py-3 border ">
-              Review the 1st para. First recite ...
-            </td>
-            <td className="px-6 py-3 border ">5 Apr 2023</td>
-            <td className="px-6 py-3 border flex justify-between">
-              <img
-                className="w-6 h-6 cursor-pointer mr-2"
-                src={update}
-                alt=""
-                title="Edit"
-              />
-              <img
-                className="w-6 h-6 cursor-pointer "
-                src={dlete}
-                alt=""
-                title="Delete"
-              />
-            </td>
-          </tr>
-          <tr className="cursor-pointer">
-            <td className="px-6 py-3 border ">1</td>
-            <td className="px-6 py-3 border ">Hefzul Quran</td>
-            <td className="px-6 py-3 border ">
-              Review the 1st para. First recite ...
-            </td>
-            <td className="px-6 py-3 border ">5 Apr 2023</td>
-            <td className="px-6 py-3 border flex justify-between">
-              <img
-                className="w-6 h-6 cursor-pointer mr-2"
-                src={update}
-                alt=""
-                title="Edit"
-              />
-              <img
-                className="w-6 h-6 cursor-pointer "
-                src={dlete}
-                alt=""
-                title="Delete"
-              />
-            </td>
-          </tr>
-          <tr className="cursor-pointer">
-            <td className="px-6 py-3 border ">1</td>
-            <td className="px-6 py-3 border ">Hefzul Quran</td>
-            <td className="px-6 py-3 border ">
-              Review the 1st para. First recite ...
-            </td>
-            <td className="px-6 py-3 border ">5 Apr 2023</td>
-            <td className="px-6 py-3 border flex justify-between">
-              <img
-                className="w-6 h-6 cursor-pointer mr-2"
-                src={update}
-                alt=""
-                title="Edit"
-              />
-              <img
-                className="w-6 h-6 cursor-pointer "
-                src={dlete}
-                alt=""
-                title="Delete"
-              />
-            </td>
-          </tr>
-          <tr className="cursor-pointer">
-            <td className="px-6 py-3 border ">1</td>
-            <td className="px-6 py-3 border ">Hefzul Quran</td>
-            <td className="px-6 py-3 border ">
-              Review the 1st para. First recite ...
-            </td>
-            <td className="px-6 py-3 border ">5 Apr 2023</td>
-            <td className="px-6 py-3 border flex justify-between">
-              <img
-                className="w-6 h-6 cursor-pointer mr-2"
-                src={update}
-                alt=""
-                title="Edit"
-              />
-              <img
-                className="w-6 h-6 cursor-pointer "
-                src={dlete}
-                alt=""
-                title="Delete"
-              />
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+      {modal && (
+        <div className="modal">
+          <div className="overlay">
+            <div className="modal-content">
+              <AddTask toggleModal={toggleModal} />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
